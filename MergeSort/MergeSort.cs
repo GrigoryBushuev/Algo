68119eb9673b70047b2cd46d72d8801d7e172457
GrigoryBushuev <grigorybushuev@hotmail.com>
2014-10-29 23:16:34 +0300

    Optimized merge sort

---
+++ MergeSort/MergeSort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+	public class MergeSort<T> where T : IComparable<in T>
+	{
+		private T[] _auxiliaryArray = null;
+
+
+		private T[] Merge(T[] arrayToSort, int lo, int mid, int hi)
+		{
+			return null;
+		}
+
+		public void Sort(T[] arrayToSort, int lo, int mid, int hi)
+		{
+
+		}
+	}
+}
