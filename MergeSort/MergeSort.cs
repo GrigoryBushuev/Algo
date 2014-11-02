754ff7a23159f7655d473087b0e4776299c784d2
GrigoryBushuev <grigorybushuev@hotmail.com>
2014-11-02 20:01:19 +0300

    MergeSort

--- MergeSort/MergeSort.cs
+++ MergeSort/MergeSort.cs
@@ -6,19 +6,64 @@ using System.Threading.Tasks;
 
 namespace MergeSort
 {
-	public class MergeSort<T> where T : IComparable<in T>
+
+
+	/// <summary>
+	/// This is optimized version of the MergeSortAlgorithm
+	/// 1. If size of array is smaller than 15 items we use InsertationSort
+	/// 2. Checking whether the array is already sorted by comparing the element with the middle index and the element with index after the middle
+	/// 3. Swapping array and auxiliary array
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	public static class MergeSortAlgorithms
 	{
-		private T[] _auxiliaryArray = null;
 
-
-		private T[] Merge(T[] arrayToSort, int lo, int mid, int hi)
+		private static void Merge<T>(T[] arrayToSort, T[] auxiliaryArray, int lo, int mid, int hi) where T : IComparable<T>
 		{
-			return null;
+			int i = lo;
+			int j = mid + 1;
+
+			//Copy elements from sorted array to auxiliary array
+			for (int n = lo; n <= hi; n++)
+				auxiliaryArray[n] = arrayToSort[n];
+
+			for (int k = lo; k <= hi; k++ )
+			{
+				//Check bounds before doing actual elements compare
+				if (i > mid)
+					arrayToSort[k] = auxiliaryArray[j++];
+				else if (j > hi)
+					arrayToSort[k] = auxiliaryArray[i++];
+				else if (auxiliaryArray[i].CompareTo(auxiliaryArray[j]) < 0)
+					arrayToSort[k] = auxiliaryArray[i++];
+				else
+					arrayToSort[k] = auxiliaryArray[j++];
+			}
 		}
 
-		public void Sort(T[] arrayToSort, int lo, int mid, int hi)
+		private static void Sort<T>(T[] arrayToSort, T[] auxiliaryArray, int lo, int hi) where T : IComparable<T>
 		{
+			//3. Check the borders 
+			if (hi <= lo)
+				return;
 
+			int mid = lo + (hi - lo) / 2;
+			Sort(arrayToSort, auxiliaryArray, lo, mid);
+			Sort(arrayToSort, auxiliaryArray, mid + 1, hi);
+			//Run actual mergin of two subarrays devided by the middle index
+			Merge(arrayToSort, auxiliaryArray, lo, mid, hi);
 		}
+
+
+		public static void MergeSort<T>(this T[] arrayToSort) where T : IComparable<T>
+		{			
+			//1. Create the auxiliary to copy sorted elements
+			var auxiliaryArray = new T[arrayToSort.Length];
+			//2. Run sorting algorithm 
+			Sort(arrayToSort, auxiliaryArray, 0,  arrayToSort.Length - 1);
+		}
+
+
+
 	}
 }
---
+++ SortUnitTest/SortUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MergeSort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace SortUnitTest
+{
+	[TestClass]
+	public class SortUnitTest
+	{
+		[TestMethod]
+		public void MergeSortTest()
+		{
+			var arrayToSort = new[] { 5, 9, 8, 1, 4, 2, 6, 5, 3, 7 };
+
+			arrayToSort.MergeSort();
+
+
+		}
+	}
+}
