78b692a873cf2ee3ef1e1d3b80dbe7a72e8fcd09
GrigoryBushuev <grigorybushuev@hotmail.com>
2014-11-16 23:47:01 +0300

    IsSorting method was added

---
+++ MergeSort/SortingUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+	public static class SortingUtils
+	{
+
+		public static bool IsSorted<T>(this T[] sortedArray, bool ascending = true) where T : IComparable<T>
+		{
+			bool isSorted = true;
+			if (sortedArray.Length == 1)
+				return true;
+
+			int comparableValue = ascending ? 1 : -1;
+
+			for (int i = 1; i < sortedArray.Length; i++)
+			{
+				var compareResult = sortedArray[i].CompareTo(sortedArray[i - 1]);
+				if (compareResult != comparableValue && compareResult != 0)
+				{
+					isSorted = false;
+					break;
+				}						
+			}
+			
+
+			return isSorted;
+		}
+	}
+}
--- SortUnitTest/SortUnitTest.cs
+++ SortUnitTest/SortUnitTest.cs
@@ -8,14 +8,24 @@ namespace SortUnitTest
 	[TestClass]
 	public class SortUnitTest
 	{
-
 		private static int[] _arrayToSort;
+		private static int[] _sortedArray;
 
 		[ClassInitialize()]
 		public static void ClassInit(TestContext context)
 		{
 			_arrayToSort = new[] { 5, 9, 8, 1, 4, 2, 6, 5, 3, 7 };
+			_sortedArray = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9};
 		}
+
+
+		[TestMethod]
+		public void IsSortedTest()
+		{
+			Assert.IsFalse(_arrayToSort.IsSorted());
+			Assert.IsTrue(_sortedArray.IsSorted());
+		}
+
 
 
 		[TestMethod]
@@ -23,10 +33,7 @@ namespace SortUnitTest
 		{
 			MergeSort<int> mergeSort = new MergeSort<int>();
 			_arrayToSort.Sort(mergeSort);
-			foreach (var item in _arrayToSort)
-			{
-		 
-			} 
+			Assert.IsTrue(_arrayToSort.IsSorted());
 		}
 	}
 }
