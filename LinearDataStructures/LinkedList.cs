f9b60a0f5e93a875d7dde8aac28c6fcaf02a15f5
GrigoryBushuev <grigorybushuev@hotmail.com>
2014-11-24 18:22:10 +0300

    Added DataStructures project

---
+++ LinearDataStructures/LinkedList.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Linear
+{
+
+	public class LinkedListNode<T>
+	{
+		public LinkedListNode(T value)
+		{
+			Value = value;
+		}
+
+		public LinkedListNode(LinkedListNode<T> next, T value)
+		{
+			Next = next;
+			Value = value;
+		}
+
+		public T Value
+		{
+			private set; 
+			get;
+		}
+
+		public LinkedListNode<T> Next
+		{
+			set; 
+			get;
+		}
+	}
+
+
+	public class LinkedList<T>
+	{
+
+		private LinkedListNode<T> _currentNode;
+
+		public LinkedList(IEnumerable<T> data)
+		{
+			foreach (var node in data)
+			{
+				
+			}
+		}
+
+		public LinkedListNode<T> AddLast(T data)
+		{
+			var newNode = new LinkedListNode<T>(data);
+			if (_currentNode == null)
+				_currentNode = newNode;
+			else
+				_currentNode.Next = newNode;
+
+			return newNode;
+		}
+
+    }
+}
