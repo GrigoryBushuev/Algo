d640aca4a523b96539dcce12b42f1dbf2332d912
GrigoryBushuev <grigorybushuev@hotmail.com>
2015-01-19 22:49:21 +0300

    IndirectSort

---
+++ MergeSort/IndirectSort..cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+	/// <summary>
+	/// A version of mergesort that does not rearrange the array, 
+	/// but returns an int[] array perm such that perm[i] is the index of the ith smallest entry in the array.
+	/// </summary>
+	public static class IndirectSort
+	{
+
+
+	}
+}
