9270ce9d35b0ca609750b15b9621b70f7b91f31b
GrigoryBushuev <grigorybushuev@hotmail.com>
2015-01-25 19:56:47 +0300

    QuickFind

---
+++ UnionFind/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnionFind
+{
+    public class QuickFind
+    {
+
+		private int[] _components;
+
+
+		private QuickFind()
+		{
+
+		}
+
+		public QuickFind (int num)
+		{
+			_components = new int[num];
+			for (int i = 0; i < num; i++)
+			{
+				_components[i] = i;
+			}
+		}
+
+
+		public bool IsConnected(int p, int q)
+		{
+			return _components[p] == _components[q];
+		}
+
+		public int Find(int p)
+		{
+			return _components[p];
+		}
+
+		public void Union(int p, int q)
+		{
+			var pId = Find(p);
+			var qId = Find(q);
+
+
+			for (int i = 0; i < _components.Length; i++)
+			{
+				if (_components[i] == pId)
+					_components[i] = qId;
+			}
+		}
+
+    }
+}
---
+++ UnionFindTest/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnionFind;
+
+namespace UnionFindTest
+{
+	[TestClass]
+	public class UnionFindUnitTest
+	{
+		[TestMethod]
+		public void QuickFindTest()
+		{
+			//Arrange 
+			var quickFind = new QuickFind(10);
+			//Act
+			quickFind.Union(4, 3);
+			quickFind.Union(3, 8);
+			quickFind.Union(6, 5);
+			quickFind.Union(9, 4);
+			quickFind.Union(2, 1);
+			quickFind.Union(8, 9);
+			quickFind.Union(5, 0);
+			quickFind.Union(7, 2);
+			quickFind.Union(6, 1);
+			quickFind.Union(1, 0);
+			quickFind.Union(6, 7);
+			//Assert
+			Assert.IsTrue(quickFind.IsConnected(4, 3));
+			Assert.IsTrue(quickFind.IsConnected(3, 8));
+			Assert.IsTrue(quickFind.IsConnected(6, 5));
+			Assert.IsTrue(quickFind.IsConnected(9, 4));
+			Assert.IsTrue(quickFind.IsConnected(2, 1));
+			Assert.IsTrue(quickFind.IsConnected(8, 9));
+			Assert.IsTrue(quickFind.IsConnected(5, 0));
+			Assert.IsTrue(quickFind.IsConnected(7, 2));
+			Assert.IsTrue(quickFind.IsConnected(6, 1));
+			Assert.IsTrue(quickFind.IsConnected(1, 0));
+			Assert.IsTrue(quickFind.IsConnected(6, 7));			
+		}
+	}
+}
