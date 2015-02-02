cc23aa9affa0cfc65288210ceb3578ae6d47b589
GrigoryBushuev <grigorybushuev@hotmail.com>
2015-02-02 18:24:42 +0300

    ShellSort

---
+++ MergeSort/ShellSort.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SortingAlgorithms;
+
+namespace MergeSort
+{
+	public class ShellSort<T> : ISortingAlgorithm<T> where T : IComparable<T>
+	{
+		public void Sort(IEnumerable<T> arrayToSort)
+		{
+			var arr = arrayToSort as T[];
+
+
+		}
+	}
+}
