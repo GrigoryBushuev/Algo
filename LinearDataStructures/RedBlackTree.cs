23e68ae3f83cedb864e62b763b85e20f09e2d4bf
GrigoryBushuev <grigorybushuev@hotmail.com>
2015-08-09 23:57:13 +0300

    Red-black tree

---
+++ LinearDataStructures/RedBlackTree.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class RedBlackTree<TNode, TKey, TValue> where TKey : IComparable<TKey> where TNode : RedBlackTreeNode<TKey, TValue>
+    {
+        private RedBlackTreeNode<TKey, TValue> _root;
+
+        private void RotateLeft()
+        {
+
+        }
+
+        private void RotateRight()
+        {
+
+        }
+
+        private void FlipColors()
+        {
+
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            Add(_root, key, value);
+        }
+
+        public RedBlackTreeNode<TKey, TValue> Add(RedBlackTreeNode<TKey, TValue> node, TKey key, TValue value)
+        {
+            if (node == null)
+                node = new RedBlackTreeNode<TKey, TValue>(key, value, null, null, 1, false);
+
+            int cmp = key.CompareTo(node.Key);
+            if (cmp < 0)
+                node.LeftNode = Add(node, key, value);
+            else if (cmp > 0)
+                node.RightNode = Add(node, key, value);
+            else
+                node.Value = value;
+
+            //Red-black magic is here
+            return node;
+        }
+    }
+}
---
+++ LinearDataStructures/RedBlackTreeNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class RedBlackTreeNode<TKey, TValue> where TKey : IComparable<TKey>
+    {
+        public TKey Key { private set; get; }
+
+        public TValue Value { set; get; }
+
+        public RedBlackTreeNode<TKey, TValue> LeftNode { set; get; }
+
+        public RedBlackTreeNode<TKey, TValue> RightNode { set; get; }
+
+        public int Size { set; get; }
+
+        public bool IsRed { set; get; }
+
+        public RedBlackTreeNode(TKey key, TValue value, RedBlackTreeNode<TKey, TValue> leftNode, RedBlackTreeNode<TKey, TValue> rightNode, int size, bool isRed = true)
+        {
+            Key = key;
+            Value = value;
+            LeftNode = leftNode;
+            RightNode = rightNode;
+            Size = size;
+            IsRed = isRed;
+        }
+    }
+}
