ad1e79a7713cef5864bf2b66fb4028e40a4cffab
GrigoryBushuev <grigorybushuev@hotmail.com>
2015-09-08 22:29:14 +0300

    Added a binary search method

--- Utils/ArrayUtils.cs
+++ Utils/ArrayUtils.cs
@@ -15,5 +15,25 @@ namespace Utils
 		    array[i] = array[j];
 		    array[j] = temp;
 	    }
+
+        public static int? Rank<T>(this T[] sortedArray, T key) where T : IComparable<T>
+        {
+            int lo = 0;
+            int hi = sortedArray.Length - 1;
+
+            while (lo <= hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+                int cmp = key.CompareTo(sortedArray[mid]);
+                if (cmp < 0)
+                    hi = mid - 1;
+                else if (cmp > 0)
+                    lo = mid + 1;
+                else
+                    return mid;
+            }
+            return null;
+        }
+
     }
 }
