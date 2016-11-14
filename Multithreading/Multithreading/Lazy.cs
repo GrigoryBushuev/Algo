5f5b173d1218b2700aa9fe1d0f5b1f6adf7f6712
GrigoryBushuev <grigorybushuev@hotmail.com>
2016-11-14 23:53:20 +0300

    put the proj in order

---
+++ Multithreading/Multithreading/Lazy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    public class MyHeavyClass<T>
+    {
+        private MyHeavyClass() { }
+
+        public MyHeavyClass<T> Instance{
+            get {
+                return LazyInitializer.EnsureInitialized<MyHeavyClass>();
+            }
+        }
+
+    }
+}
